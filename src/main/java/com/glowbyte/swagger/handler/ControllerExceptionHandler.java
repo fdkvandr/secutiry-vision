b55c93df0557f51a31953b122ea5b73aca6d0ccd
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-07-14 22:59:10 +0300

    Add controller advice for WebClientResponseException

---
+++ src/main/java/com/glowbyte/swagger/handler/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.glowbyte.swagger.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(WebClientResponseException.class)
+    protected ResponseEntity<Object> handleWebClientResponseException(
+            WebClientResponseException ex) {
+
+        log.error(ex.getClass().getCanonicalName(), ex);
+        return new ResponseEntity<>(ex.getMessage(), ex.getHeaders(), ex.getStatusCode());
+    }
+}
