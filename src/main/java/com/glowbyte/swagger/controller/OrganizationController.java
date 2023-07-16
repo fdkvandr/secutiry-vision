4f2ccc432589bac08fdec89dc199f80ea1b8d31b
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-07-16 14:50:20 +0300

    Refactored

--- src/main/java/com/glowbyte/swagger/controller/OrganizationController.java
+++ src/main/java/com/glowbyte/swagger/controller/OrganizationController.java
@@ -4,6 +4,7 @@ import com.glowbyte.swagger.dto.OrganizationDto;
 import com.glowbyte.swagger.service.OrganizationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class OrganizationController {
     private final OrganizationService organizationService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/reference/create", produces = MediaType.APPLICATION_JSON_VALUE)
     public String create(@RequestBody OrganizationDto organizationDto) {
         return organizationService.create(organizationDto);
--- src/main/java/com/glowbyte/swagger/handler/ControllerExceptionHandler.java
+++ src/main/java/com/glowbyte/swagger/handler/ControllerExceptionHandler.java
@@ -2,13 +2,13 @@ package com.glowbyte.swagger.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(WebClientResponseException.class)
