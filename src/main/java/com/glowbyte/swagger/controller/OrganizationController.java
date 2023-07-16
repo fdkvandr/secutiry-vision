8836e1849b1ce797ae917d15b21d789a5e51e991
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-07-16 14:45:25 +0300

    Refactored

--- src/main/java/com/glowbyte/swagger/controller/OrganizationController.java
+++ src/main/java/com/glowbyte/swagger/controller/OrganizationController.java
@@ -4,6 +4,7 @@ import com.glowbyte.swagger.dto.OrganizationDto;
 import com.glowbyte.swagger.service.OrganizationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class OrganizationController {
     private final OrganizationService organizationService;
 
-    @PostMapping("/reference/create")
+    @PostMapping(value = "/reference/create", produces = MediaType.APPLICATION_JSON_VALUE)
     public String create(@RequestBody OrganizationDto organizationDto) {
         return organizationService.create(organizationDto);
     }
